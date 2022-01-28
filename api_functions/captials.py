5e42f12369be91e43c30ca9e75983664c0c0d3fa
Mark McNally <mark@mcnally.je>
2022-01-28 19:19:57 +0000

    initial commit

---
+++ api_functions/captials.py
@@ -0,0 +1,90 @@
+import json
+import random
+
+# TODO:
+# load json data
+# sort it correctly
+# generate a questions
+
+
+
+def load_json():
+    with open('../data/country-list-with-ids.json', 'r') as file:
+        data=file.read()
+        return json.loads(data)
+
+
+def get_random_capital(data):
+    # how to get a captial
+    # data[NUM]['country']
+    # data[NUM]['capital']
+    # list goes out of bounds if we hit the acutally max num due to indexing at 0
+    max_num = len(data) - 1
+    random_num = random.randrange(0, max_num)
+    question = {'country': data[random_num]['country'],
+                'capital': data[random_num]['capital'],
+                }
+
+    return question
+
+def get_choices(data, question):
+    max_num = len(data) - 1
+    # should init choices in first func with correct
+    choices = []
+    i = 0
+    while i < 3:
+        random_num = random.randrange(0, max_num)
+        country = data[random_num]['capital']
+        if country == question['capital']:
+            continue
+        elif country in choices:
+            continue
+        else:
+            choices.append(country)
+            i = i + 1
+
+
+    return choices
+
+
+def gen_rand_choices(question, choices):
+    country = question['capital']
+    choices.append(country)
+    random.shuffle(choices)
+
+
+    return choices
+
+
+
+def make_json(question, rand_choices):
+    country = question['country']
+    response = {
+              'question': f'What is the Capital of {country}',
+              'choices': rand_choices,
+              'correct': question['capital'],
+              }
+    return response
+
+
+
+
+
+
+
+
+
+
+if __name__ == "__main__":
+    data = load_json()
+    question = get_random_capital(data)
+    choices = get_choices(data, question)
+#    print(choices)
+    rand_choices = gen_rand_choices(question, choices)
+#    print(rand_choices)
+    correct = question['country']
+    print(make_json(question, rand_choices))
+
+
+
+
---
+++ readme.md
@@ -0,0 +1,91 @@
+ A trivia app
+
+
+## routes
+
+Question
+category
+
+## categories
+
+
+Captial cities
+    * Of US states
+    * Of Europe
+    * Africa
+    * Of Americas
+    * Asia
+
+Countries
+
+Flags
+
+General Knowlege
+
+Famous Landmarks (ken)
+
+
+### return (json)
+
+Question
+Answer
+LIst of wrong answers (including the answer, abcd)
+Optional image
+
+
+## Full example of what we want
+
+* General Knowledge
+```
+curl example.com/api/trivia?cat=1
+
+{
+question: 'what is the best energy drink',
+answer: 'monster'
+choices: '['monster','lucazade','water','red bull']
+}
+
+```
+
+* Cities
+
+```
+curl example.com/api/trivia?cat=2
+
+{
+question: 'What is the capital of the UK',
+answer: 'London'
+choices: '['london','Amsterdam','Kiev','Helsinki']
+}
+```
+
+## TODO
+
+* Captial cities
+* list of landmarks
+
+### Create the function to generate the captial cities question 
+
+
+* genrate question ✅
+* Fix relative path
+* Add continent (Future feature) [x]
+
+
+### Create a basic api 
+
+
+
+
+
+## resources
+
+* State capital
+  <https://github.com/jasperdebie/VisInfo/blob/master/us-state-capitals.csv>
+
+
+
+
+
+
+
